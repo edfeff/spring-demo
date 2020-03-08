7362240f90e9cb156ebc42c4ec50c5bacdf80f06
edfeff <396859442@qq.com>
2020-03-08 10:12:59 +0800

    add 权限注解和权限表达式示例

--- spring-security-cat/src/main/java/com/wpp/spring/security/ant/controller/ManagerController.java
+++ spring-security-cat/src/main/java/com/wpp/spring/security/ant/controller/ManagerController.java
@@ -1,9 +1,15 @@
 package com.wpp.spring.security.ant.controller;
 
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.access.prepost.PreFilter;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author wangpp
@@ -16,4 +22,17 @@ public class ManagerController {
     public String progress() {
         return "manager finish 80%";
     }
+
+    @PreAuthorize( "#id<10 and principal.username.equals(#username) or #user.username.equals('admin')" )
+    @PostAuthorize( "returnObject%2==0" )
+    public Integer test(Integer id, String username, User user) {
+        return id;
+    }
+
+    @PreFilter( "filterObject%2==0" )
+    @PostFilter( "filterObject%3==0" )
+    public List<Integer> test2(List<Integer> list) {
+        return list;
+    }
+
 }
