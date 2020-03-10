e7044bf48382db20765c2865abe5b5472171f534
edfeff <396859442@qq.com>
2020-03-10 23:49:41 +0800

    处理乱码问题

--- springmvc-template/src/main/java/com/wpp/controller/TestController.java
+++ springmvc-template/src/main/java/com/wpp/controller/TestController.java
@@ -1,6 +1,7 @@
 package com.wpp.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,7 +16,18 @@ public class TestController {
     @RequestMapping( "/hello" )
     @ResponseBody
     public String hello() {
-        log.info("hello");
-        return "hello";
+        log.info("hello，世界");
+        return "hello，世界";
+    }
+
+    /**
+     * 控制器返回结果乱码- 解决方法1
+     *
+     * @return
+     */
+    @RequestMapping( value = "/order", produces = "text/plain;charset=utf-8" )
+    @ResponseBody
+    public String addOrder() {
+        return "成功";
     }
 }
---
+++ springmvc-template/src/main/webapp/index.jsp
@@ -0,0 +1,6 @@
+<%@ page pageEncoding="UTF-8" contentType="text/html; charset=UTF-8" language="java" %>
+<html>
+<body>
+<h2>Hello World!世界</h2>
+</body>
+</html>
